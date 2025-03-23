d6cb23f0b3b3f24d8b9a9ecdf53b3a2a4fcdf9a0
Paul-Ben <paulben.ajene@gmail.com>
2025-03-23 15:34:41 +0000

    update

--- resources/views/superadmin/organisations/index.blade.php
+++ resources/views/superadmin/organisations/index.blade.php
@@ -20,7 +20,7 @@
             <div class="bg-light text-center rounded p-4">
                 <div class="d-flex align-items-center justify-content-between mb-4">
                     <h6 class="mb-0">Organisation Management</h6>
-                    <div>
+                    {{-- <div>
                         <form method="GET" action="{{route('search.org')}}">
                             <div class="input-group mb-3">
                                 <input type="text" name="search" class="form-control" placeholder="Search items..."
@@ -28,7 +28,7 @@
                                 <button class="btn btn-primary" type="submit">Search</button>
                             </div>
                         </form>
-                    </div>
+                    </div> --}}
                     <div>
                         <a class="btn btn-sm btn-primary" href="{{ route('organisation.create') }}">Add Tenant</a>
                         <a class="btn btn-sm btn-primary" href="{{ url()->previous() }}"><i
@@ -37,7 +37,7 @@
 
                 </div>
                 <div class="table-responsive">
-                    <table class="table text-start align-middle table-bordered table-hover mb-0">
+                    <table id="superOrganisation" class="table text-start align-middle table-bordered table-hover mb-0">
                         <thead>
                             <tr class="text-dark">
                                 <th scope="col">#</th>
@@ -83,4 +83,28 @@
             </div>
         </div>
         <!-- Table End -->
+        <script>
+            $(document).ready(function() {
+                $('#superOrganisation').DataTable({
+                    responsive: true,
+                    autoWidth: false,
+                    paging: true, // Enable pagination
+                    searching: true, // Enable search
+                    ordering: true, // Enable sorting
+                    lengthMenu: [10, 25, 50, 100], // Dropdown for showing entries
+                    columnDefs: [{
+                            orderable: false,
+                            targets: -1
+                        } // Disable sorting on last column (Actions)
+                    ],
+                    language: {
+                        searchPlaceholder: "Search here...",
+                        zeroRecords: "No matching records found",
+                        lengthMenu: "Show entries",
+                        // info: "Showing START to END of TOTAL entries",
+                        infoFiltered: "(filtered from MAX total entries)",
+                    }
+                });
+            });
+        </script>
     @endsection
