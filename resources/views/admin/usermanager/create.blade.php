f02656cffe5455bef53def310f20b67b4abfa6e1
Paul-Ben <paulben.ajene@gmail.com>
2025-03-24 10:05:05 +0000

    fix

--- resources/views/admin/usermanager/create.blade.php
+++ resources/views/admin/usermanager/create.blade.php
@@ -5,12 +5,18 @@
         <div class="container-fluid pt-4 px-4">
             <div class="col-12">
                 <div class="bg-light rounded h-100 p-4">
-                    @if (session::has($errors))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            <i class="fa fa-exclamation-circle me-2"></i>{{ $errors->first() }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @endif
+                    @if (session('errors'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        
+                        <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                    </ul>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                
+            @endif
                 </div>
             </div>
         </div>
